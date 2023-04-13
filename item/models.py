5863183f3a21fcb0b306519014d771c051e8f1df
mulatmek <mulatmek@gmail.com>
2023-04-13 21:11:26 +0300

    add details option for each item

--- item/models.py
+++ item/models.py
@@ -3,17 +3,19 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
     class Meta:
         ordering = ('name',)
         verbose_name_plural = 'Categories'
     
     def __str__(self):
         return self.name
-class Item (models.Model):
+
+class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.FloatField()
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
--- item/views.py
+++ item/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_objext_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Item
+
 def detail(request, item_id):
-    item = get_objext_or_404(Item, pk=item_id)
+    item = get_object_or_404(Item, pk=item_id)
     return render(request, 'item/detail.html', {
         'item': item
     })
