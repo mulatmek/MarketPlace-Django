7fa7ce21e4e9305e1d3bad66ec847e29a3da8e69
mulatmek <mulatmek@gmail.com>
2023-04-13 20:57:56 +0300

    add templates item dir with form,detail,items html files

--- core/views.py
+++ core/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 import os
-# Create your views here.
+from item.models import Item, Category
 def index(request):
-    return render(request, os.path.join('core', 'index.html'))
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, os.path.join('core', 'index.html'),{
+        'items': items,
+        'categories': categories
+    })
+
 
 def contact(request):
     return render(request, os.path.join('core', 'contact.html'))
--- item/views.py
+++ item/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_objext_or_404
+from .models import Item
+def detail(request, item_id):
+    item = get_objext_or_404(Item, pk=item_id)
+    return render(request, 'item/detail.html', {
+        'item': item
+    })
