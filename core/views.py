617734e3019a4adc83e920b702756eb9774e7f56
mulatmek <mulatmek@gmail.com>
2023-04-17 21:40:07 +0300

    signup page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import os
 from item.models import Item, Category
+from .forms import SignupForm
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
@@ -12,3 +13,7 @@ def index(request):
 
 def contact(request):
     return render(request, os.path.join('core', 'contact.html'))
+
+def signup(request):
+    form = SignupForm()
+    return render(request,os.path.join('core', 'signup.html'), context={'form': form})
--- item/views.py
+++ item/views.py
@@ -3,6 +3,8 @@ from .models import Item
 
 def detail(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
+    related_items = Item.objects.filter(category=item.category,is_sold =False).exclude(pk=item_id)[0:3]
     return render(request, 'item/detail.html', {
-        'item': item
+        'item': item,
+        'related_items': related_items,
     })
